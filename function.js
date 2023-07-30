0a1f407ad8063dc28565eef6e48d225ad89c4475
dal10000 <dal10000@hanmail.net>
2023-07-30 11:46:55 +0900

    function1

---
+++ function.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//Function
+// - fundamental building block in the program
+// - subprogram can be used multiple times
+// - perform a task or calculates a value
+
+// 1. Function declaration
+// function name(param1, param2){body... return;}
+// one function === one thing
+// naming: doSomething, commang, verb
+// e.g. createCardAndPoint -> createCard, createPoint
+// function is object in JS
