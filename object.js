189493c8180fe5112eefe29b859ed3c936eac9b1
dal10000 <dal10000@hanmail.net>
2023-08-03 09:45:24 +0900

    array

--- object.js
+++ object.js
@@ -59,4 +59,38 @@ console.log("random" in ellie);
 console.log(ellie.random);
 
 // 6. for..in vs for..of
-// for(key in obj)
+// for (key in obj)
+for (const key in ellie) {
+  console.log(key);
+}
+
+// for (value of iterable)
+const array = [1, 2, 4, 5];
+for (const value of array) {
+  console.log(value);
+}
+
+// 7. Fun cloning
+// Object.assign(dest, [obj1, obj2, obj3...])
+const user = { name: "ellie", age: "20" };
+const user2 = user;
+
+console.log(user.name);
+
+// old way
+const user3 = {};
+for (const key in user) {
+  user3[key] = user[key];
+}
+console.log(user3);
+
+// new way
+const user4 = Object.assign({}, user);
+console.log(user4);
+
+//another example
+const fruit1 = { color: "red" };
+const fruit2 = { color: "blue", size: "big" };
+const fruit3 = { color: "black" };
+const mixed = Object.assign({}, fruit1, fruit2, fruit3);
+console.log(mixed);
